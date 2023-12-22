1541764b80fa383aaa7cb28fa6fb6c370ae55d43
BurntDisc <ellistonfranks@gmail.com>
2023-12-21 21:42:39 -0800

    first commit

---
+++ Source/Zippy/ZippyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZippyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZippyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZippyGameMode();
+};
+
+
+
